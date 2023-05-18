2378718a0e512ba9939117259c0c7b4bd813c4ac
JoyMondal007-web <120724151+JoyMondal007-web@users.noreply.github.com>
2023-05-18 13:53:17 +0600

    first commit

---
+++ dict/script.js
@@ -0,0 +1,67 @@
+const generatePasswordBtn = document.querySelector('.generate-passwords-btn')
+const displayPasswords = document.querySelector('.display-passwords-container')
+const displayPassword =document.querySelectorAll('.pw-display')
+const changePasswordLength = document.querySelector('.change-password-length')
+
+//const one=document.querySelector('.one')
+const alart=document.querySelector('.alart')
+
+ 
+
+const characters =[
+    'A','B','C','D','E','F','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','n','o','p','q','r','s','t','u','v','w','x','y','z','*','&','$','#','!','?','<','>','+'
+    ]
+    
+function generatePassword(num=8){
+    const pwArr =[]
+      
+    for(let j=1; j <=4; j++)
+    {
+        let resultStr=""
+        for(let i=1; i <= num; i++)
+        {
+            let randomIndex = Math.floor(Math.random() * characters.length) 
+            resultStr += characters[randomIndex]
+        }
+      pwArr.push(resultStr) 
+    }
+      return pwArr
+ }
+    
+let num = changePasswordLength.min
+changePasswordLength.value = num
+changePasswordLength.addEventListener('input' , () =>  num = changePasswordLength.value)
+   
+     
+function renderPassword(){
+    
+let passwords =  generatePassword(num)
+for(let i=0; i < displayPassword.length; i++){
+    
+  // displayPassword[i].innerHTML = `${passwords[i]}<i class="fa-regular fa-copy"></i>`
+   displayPassword[i].value = passwords[i]
+  } 
+}
+
+generatePasswordBtn.addEventListener('click' , renderPassword)
+
+const copyBtn=document.querySelectorAll('.fa-copy')
+      copyBtn.forEach(function(copy){
+        copy.addEventListener('click',function(e){
+          let target=e.currentTarget.previousElementSibling;
+          //console.log(target)
+          target.select();
+          navigator.clipboard.writeText(target.value)
+          if(target.value){
+            alart.innerHTML=`${target.value} is coppied!`
+            setTimeout(function(){
+              alart.classList.add('show-alart')
+           }, 2000);
+            
+          }else{
+            alart.classList.remove('show-alart')
+          }
+          
+    })
+
+  })
